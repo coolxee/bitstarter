704e13db5267d71a14141cd161fa503acd221f9f
Xeno <coolxee@gmail.com>
2014-03-10 07:02:58 +0000

    modified web.js

--- web.js
+++ web.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var app = express();
+var fileName = "index.html"
+
+var myBuf = fs.readFileSync(fileName)
+var myStr = myBuf.toString()
+
 app.use(express.logger());
 
+
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send(myStr);
 });
 
 var port = process.env.PORT || 5000;
